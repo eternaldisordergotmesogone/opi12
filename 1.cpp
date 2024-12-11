593a80ef88e0e6b9781504860fcea1f2b7bfc567
Volodymyr Kameniev <vladimirkamenev22@gmail.com>
2024-12-12 00:56:07 +0200

    new file

---
+++ 1.cpp
@@ -0,0 +1,5 @@
+cout << '1' << '2' << '3' << endl;
+cout << setw(15) << showpos << right << 4 << endl;
+cout << setw(20) << internal << showpos << -67.09 << endl;
+cout << setw(15) << right << showpos << 235 << endl;
+cout << '8' << 'a' << '1' << endl;
