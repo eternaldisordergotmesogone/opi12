eed399695617f0308c842c2104f8cfa35f30f532
Volodymyr Kameniev <vladimirkamenev22@gmail.com>
2024-12-12 00:59:33 +0200

    new file

--- 1.cpp
+++ 1.cpp
@@ -3,3 +3,7 @@ cout << setw(15) << showpos << right << 4 << endl;
 cout << setw(20) << internal << showpos << -67.09 << endl;
 cout << setw(15) << right << showpos << 235 << endl;
 cout << '8' << 'a' << '1' << endl;
+
+const n = 5;
+const m = 3;
+y+x;
